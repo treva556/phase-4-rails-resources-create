2e717da5a27171c6aa54539dba7a283cb11c0725
treva556 <marktreva92@gmail.com>
2023-03-17 18:54:20 +0300

    done

--- db/migrate/20210502132944_create_birds.rb
+++ db/migrate/20210502132944_create_birds.rb
@@ -1,10 +1,6 @@
-class CreateBirds < ActiveRecord::Migration[6.1]
-  def change
-    create_table :birds do |t|
-      t.string :name
-      t.string :species
-
-      t.timestamps
-    end
-  end
+create_table "birds", force: :cascade do |t|
+  t.string "name"
+  t.string "species"
+  t.datetime "created_at", precision: 6, null: false
+  t.datetime "updated_at", precision: 6, null: false
 end
